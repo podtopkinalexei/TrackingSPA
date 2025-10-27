bfcc0948b887bff35c5b5a6c2d083cfb1260d928
podtopkinalexei <podtopkinalexei@gmail.com>
2025-10-27 20:16:17 +0300

    Обновлены пути users

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
-from rest_framework import generics, permissions, status
-from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
-from .models import User
+from rest_framework import generics, permissions, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+
 from .serializers import UserRegistrationSerializer, UserProfileSerializer
 
 
